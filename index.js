fe1d1bc14c1ad88dfa3e63b2b73ae0a8c9e356f3
shreyas-is-the-best-at-coding <146784477+shreyas-is-the-best-at-coding@users.noreply.github.com>
2024-05-12 08:51:16 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+<html>
+
+  <head>
+    <link rel="stylesheet" href="s.css">
+    <script src="s.js"></script>
+    <script src="p.js"></script>
+  </head>
+
+  <body>
+  
+    <div id="outside">
+    
+      <p id="outside-title">
+        Happy Mothers Day Aai!!!!
+      </p>
+
+      <img id="outside-pic" src="girl.png">
+
+      <p>
+        <button onclick="open_card()">Click to open</button>
+      </p>
+
+    </div>
+  
+    <div id="inside">
+    
+      <p id="inside-title">
+        Happy Mothers Day Aai!!!!
+      </p>
+      
+      <img id="inside-pic" src="diamond.png">
+      
+      <p id="message">
+        मातृदिनाच्या शुभेच्छा!!
+      </p>
+
+      <p>
+        <button onclick="close_card()">Click to close</button>
+      </p>
+      
+
+    </div>
+    
+  </body>
+  
+</html>
